db98d7a2b1a4b56719297b8ed692a7a855bb02cb
Sidio01 <sidio1992@gmail.com>
2020-07-05 15:24:22 +0300

    Finished C3W2

--- Programmirovanie-na-Python-Specialization/sozdanie_web-servisov_na_python/week_2/test.py
+++ Programmirovanie-na-Python-Specialization/sozdanie_web-servisov_na_python/week_2/test.py
@@ -1,11 +1,17 @@
 import requests
 from decimal import Decimal
-from currency import convert
+# from currency import convert
 
 
-correct = Decimal('3754.8057')
-result = convert(Decimal("1000.1000"), 'RUR', 'JPY', "17/02/2005", requests)
-if result == correct:
-    print("Correct")
-else:
-    print("Incorrect: %s != %s" % (result, correct))
+# correct = Decimal('3754.8057')
+# result = convert(Decimal("1000.1000"), 'RUR', 'JPY', "17/02/2005", requests)
+# if result == correct:
+#     print("Correct")
+# else:
+#     print("Incorrect: %s != %s" % (result, correct))
+
+
+rur_val = 26.6352
+rur_nom = 100
+x = rur_nom / rur_val
+print(x * 1000.1000)
